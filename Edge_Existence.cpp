ef6287e65197095e9a8942e0161b974c0fb4cbe1
Shahadat Hossen <60475567+ShahadatHossen53@users.noreply.github.com>
2020-02-05 01:20:12 +0600

    Create Edge_Existence.cpp

---
+++ Edge_Existence.cpp
@@ -0,0 +1,37 @@
+ #include <iostream>
+
+    using namespace std;
+
+    bool A[1000][1000];
+
+    void initialize()
+    {
+        for(int i = 0;i < 10;++i)
+            for(int j = 0;j < 10;++j)
+                A[i][j] = false;
+    }
+    int main()
+    {
+        int x, y, nodes, edges;
+        initialize();
+        cin >> nodes;
+        cin >> edges;
+        for(int i = 0;i < edges;++i)
+        {
+            cin >> x >> y;
+            A[x-1][y-1] = true;
+            A[y-1][x-1] = true;
+       }
+        int query, a, b;
+        cin>>query;
+        for(int i = 0; i<query; i++){
+            cin>>a>>b;
+            if(A[a-1][b-1]){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+      return 0;
+}
